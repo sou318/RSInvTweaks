7278daa32232a1ca5ec6f6df58c53a624b64fbee
sou <sou.suke.i0318@gmail.com>
2024-12-01 03:25:53 +0900

    modが有効でも検索タブには出てくるように

--- src/client/java/sou/rsinvtweaks/client/mixin/MItemGroups.java
+++ src/client/java/sou/rsinvtweaks/client/mixin/MItemGroups.java
@@ -1,20 +1,24 @@
 package sou.rsinvtweaks.client.mixin;
 
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import sou.rsinvtweaks.client.RsinvtweaksClient;
 
 @Mixin(ItemGroups.class)
 abstract class MItemGroups
 {
-    @Inject(method = "method_51330", at =@At(value="HEAD"), cancellable=true)
-    private static void inject(ItemGroup.DisplayContext displayContext, ItemGroup.Entries entries, CallbackInfo ci)
+    @Redirect(method = "method_51330", at = @At(value="INVOKE", target="Lnet/minecraft/item/ItemGroup$Entries;add(Lnet/minecraft/item/ItemConvertible;)V"))
+    private static void inject(ItemGroup.Entries instance, ItemConvertible item)
     {
-        if (!RsinvtweaksClient.config.enableMod) return;
-        ci.cancel();
+        instance.add(
+            item,
+            RsinvtweaksClient.config.enableMod
+                ? ItemGroup.StackVisibility.SEARCH_TAB_ONLY
+                : ItemGroup.StackVisibility.PARENT_AND_SEARCH_TABS
+        );
     }
 }
