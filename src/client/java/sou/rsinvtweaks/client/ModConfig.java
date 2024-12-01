499649aa9ac565c6fd11293c3280e9e35d0d260b
sou <sou.suke.i0318@gmail.com>
2024-12-01 18:13:40 +0900

    設定ファイル分割の為、一旦コミット

--- src/client/java/sou/rsinvtweaks/client/ModConfig.java
+++ src/client/java/sou/rsinvtweaks/client/ModConfig.java
@@ -19,9 +19,7 @@ public class ModConfig implements ConfigData, ModMenuApi
 
     public boolean enableMod  = true;
 
-    // WOOLS
-    @ConfigEntry.Category("wool") boolean
-    enableWool      = true,
+    @ConfigEntry.Category("wool") boolean wool_enable = true,
     wool_white      = true,
     wool_orange     = true,
     wool_magenta    = true,
@@ -38,5 +36,49 @@ public class ModConfig implements ConfigData, ModMenuApi
     wool_green      = true,
     wool_red        = true,
     wool_black      = true
+        ;
+
+    // 自ら信号を出す物のみ
+    @ConfigEntry.Category("input") boolean
+    input_target = true,
+    input_lever = true,
+    input_oak_button = true,
+    input_stone_button = true,
+    input_oak_pressure_plate = true,
+    input_stone_pressure_plate = true,
+    input_light_weighted_pressure_plate = true,
+    input_heavy_weighted_pressure_plate = true,
+    input_trapped_chest = true,
+    input_tripwire_hook = true,
+    input_daylight_detector = true,
+    input_sculk_sensor = true,
+    input_calibrated_suculk_sensor = true
+        ;
+
+    @ConfigEntry.Category("minecart") boolean minecart_enable = true,
+    minecart_minecart = true,
+    minecart_hopper_minecart = true,
+    minecart_chest_minecart = true,
+    minecart_furnace_minecart = true,
+    minecart_tnt_minecart = true,
+    minecart_rail = true,
+    minecart_powered_rail = true,
+    minecart_detector_rail = true,
+    minecart_activator_rail = true
+        ;
+
+    @ConfigEntry.Category("inventory") boolean inventory_enable = true,
+    inventory_chest = true,
+    inventory_barrel = true,
+    inventory_hopper = true,
+    inventory_dropper = true,
+    inventory_dispenser = true,
+    inventory_crafter = true,
+    inventory_composter = true,
+    inventory_cauldron = true,
+    inventory_furnace = true,
+    inventory_chiseled_bookshelf = true,
+    inventory_decorated_pot = true,
+    inventory_jukebox = true
         ;
 }
--- src/client/java/sou/rsinvtweaks/client/RsinvtweaksClient.java
+++ src/client/java/sou/rsinvtweaks/client/RsinvtweaksClient.java
@@ -30,24 +30,54 @@ public class RsinvtweaksClient implements ClientModInitializer
                 add(true, Items.REDSTONE_TORCH);
                 add(true, Items.REPEATER);
                 add(true, Items.COMPARATOR);
+                add(true, Items.REDSTONE_BLOCK);
 
-                if (config.enableWool) {
-                    add(config.wool_white       , Items.WHITE_WOOL      );
-                    add(config.wool_orange      , Items.ORANGE_WOOL     );
-                    add(config.wool_magenta     , Items.MAGENTA_WOOL    );
-                    add(config.wool_lightBlue   , Items.LIGHT_BLUE_WOOL );
-                    add(config.wool_yellow      , Items.YELLOW_WOOL     );
-                    add(config.wool_lime        , Items.LIME_WOOL       );
-                    add(config.wool_pink        , Items.PINK_WOOL       );
-                    add(config.wool_gray        , Items.GRAY_WOOL       );
-                    add(config.wool_lightGray   , Items.LIGHT_GRAY_WOOL );
-                    add(config.wool_cyan        , Items.CYAN_WOOL       );
-                    add(config.wool_purple      , Items.PURPLE_WOOL     );
-                    add(config.wool_blue        , Items.BLUE_WOOL       );
-                    add(config.wool_brown       , Items.BROWN_WOOL      );
-                    add(config.wool_green       , Items.GREEN_WOOL      );
-                    add(config.wool_red         , Items.RED_WOOL        );
-                    add(config.wool_black       , Items.BLACK_WOOL      );
+                if (config.wool_enable)
+                {
+                    add(config.wool_white, Items.WHITE_WOOL);
+                    add(config.wool_orange, Items.ORANGE_WOOL);
+                    add(config.wool_magenta, Items.MAGENTA_WOOL);
+                    add(config.wool_lightBlue, Items.LIGHT_BLUE_WOOL);
+                    add(config.wool_yellow, Items.YELLOW_WOOL);
+                    add(config.wool_lime, Items.LIME_WOOL);
+                    add(config.wool_pink, Items.PINK_WOOL);
+                    add(config.wool_gray, Items.GRAY_WOOL);
+                    add(config.wool_lightGray, Items.LIGHT_GRAY_WOOL);
+                    add(config.wool_cyan, Items.CYAN_WOOL);
+                    add(config.wool_purple, Items.PURPLE_WOOL);
+                    add(config.wool_blue, Items.BLUE_WOOL);
+                    add(config.wool_brown, Items.BROWN_WOOL);
+                    add(config.wool_green, Items.GREEN_WOOL);
+                    add(config.wool_red, Items.RED_WOOL);
+                    add(config.wool_black, Items.BLACK_WOOL);
+                }
+
+                add(config.input_target, Items.TARGET);
+                add(config.input_lever, Items.LEVER);
+                add(config.input_oak_button, Items.OAK_BUTTON);
+                add(config.input_stone_button, Items.STONE_BUTTON);
+                add(config.input_oak_pressure_plate, Items.OAK_PRESSURE_PLATE);
+                add(config.input_stone_pressure_plate, Items.STONE_PRESSURE_PLATE);
+                add(config.input_light_weighted_pressure_plate, Items.LIGHT_WEIGHTED_PRESSURE_PLATE);
+                add(config.input_heavy_weighted_pressure_plate, Items.HEAVY_WEIGHTED_PRESSURE_PLATE);
+                add(config.input_trapped_chest, Items.TRAPPED_CHEST);
+                add(config.input_tripwire_hook, Items.TRIPWIRE_HOOK);
+                add(config.input_tripwire_hook, Items.STRING);
+                add(config.input_daylight_detector, Items.DAYLIGHT_DETECTOR);
+                add(config.input_sculk_sensor, Items.SCULK_SENSOR);
+                add(config.input_calibrated_suculk_sensor, Items.CALIBRATED_SCULK_SENSOR);
+
+                if (config.minecart_enable)
+                {
+                    add(config.minecart_minecart, Items.MINECART);
+                    add(config.minecart_hopper_minecart, Items.HOPPER_MINECART);
+                    add(config.minecart_chest_minecart, Items.CHEST_MINECART);
+                    add(config.minecart_furnace_minecart, Items.FURNACE_MINECART);
+                    add(config.minecart_tnt_minecart, Items.TNT_MINECART);
+                    add(config.minecart_rail, Items.RAIL);
+                    add(config.minecart_powered_rail, Items.POWERED_RAIL);
+                    add(config.minecart_detector_rail, Items.DETECTOR_RAIL);
+                    add(config.minecart_activator_rail, Items.ACTIVATOR_RAIL);
                 }
             }}.register(entries);
         });
